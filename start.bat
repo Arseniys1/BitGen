a519876a5e2654250371891e3b72cafe14383f26
Arseniys1 <thevalakas1@gmail.com>
2023-11-27 20:05:24 +0500

    FIX

--- start.bat
+++ start.bat
@@ -9,7 +9,7 @@ IF EXIST ".\venv" (
     .\venv\Scripts\python.exe main.py --proxy0 %privatekeeperapitoken% --timeout %timeout% --threads %threads%
 ) ELSE (
     echo Use global python
-    python.exe main.py --proxy0 QUJ9-LRX1-VI6Y-A5ND-8XD3-NG33-AEN6-DFJB-MRF8 --timeout %timeout% --threads %threads%
+    python.exe main.py --proxy0 %privatekeeperapitoken% --timeout %timeout% --threads %threads%
 )
 
 pause
